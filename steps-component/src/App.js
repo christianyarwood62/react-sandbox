155e185684e105c346f630319fed19988c3392d8
Christian Yarwood <christianyarwood@gmail.com>
2025-07-14 11:00:18 +0100

    Add correct use of state management to funcs

--- steps-component/src/App.js
+++ steps-component/src/App.js
@@ -12,17 +12,16 @@ export default function App() {
 
   function handlePrevious() {
     if (step > 1) {
-      setStep(step - 1);
+      setStep((s) => s - 1);
     }
   }
 
   function handleNext() {
     if (step < 3) {
-      setStep(step + 1);
+      setStep((s) => s + 1);
+      // setStep(s + 1); this works but you shouldnt update state based on current state, use a callback instead
     }
   }
-
-
 
   return (
     <div>
