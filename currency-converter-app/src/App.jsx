100a120dfc0cf7329794cc401aa002772e8799f3
Christian Yarwood <christianyarwood@gmail.com>
2025-09-04 11:35:46 +0100

    Add display message to correct inputs

--- currency-converter-app/src/App.jsx
+++ currency-converter-app/src/App.jsx
@@ -1,14 +1,41 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function App() {
   const [input, setInput] = useState("");
   const [firstCurrency, setFirstCurrency] = useState("INR");
   const [secondCurrency, setSecondCurrency] = useState("CAD");
+  const [output, setOutput] = useState("No conversion yet");
 
   function handleInput(e) {
-    setInput(e.target.value);
+    setInput(Number(e.target.value));
+  }
+
+  useEffect(
+    function () {
+      async function fetchCurrencyConversions() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${input}&from=${firstCurrency}&to=${secondCurrency}`
+        );
+
+        const conversions = await res.json();
+        if (firstCurrency === secondCurrency || !input) return;
+
+        console.log(conversions);
+      }
+
+      fetchCurrencyConversions();
+    },
+    [input, firstCurrency, secondCurrency]
+  );
+
+  let displayMessage;
+  if (firstCurrency === secondCurrency) {
+    displayMessage = "Change currency";
+  } else if (!input) {
+    displayMessage = "Input a number";
   }
 
   return (
@@ -32,7 +59,7 @@ export default function App() {
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{(firstCurrency === secondCurrency || !input) && displayMessage}</p>
     </div>
   );
 }
